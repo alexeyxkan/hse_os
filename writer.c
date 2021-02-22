e1cdd4fb1398520902710f4fc4ee28205ae4414f
alexey <akan@edu.hse.ru>
2021-02-22 14:26:08 +0300

    Lab 6 done

---
+++ writer.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int fd, result; 
+	size_t size; 
+	char resstring[14]; 
+	char name[] = "file.fifo";
+
+	(void)umask(0);
+
+	printf("I'll write data to fifo\n");
+
+	if((fd = open(name, O_WRONLY)) < 0) {
+		printf("Can\'t open fifo for writing\n");
+		exit(-1);
+	}
+
+	size = write(fd, "Hello, world!", 14);
+	printf("I wrote to fifo\n");
+
+	if(size != 14) {
+		printf("Can\'t write all string to fifo\n");
+		exit(-1);
+	}
+
+	close(fd);
+	printf("Writer exit\n");
+
+	return 0;
+}
